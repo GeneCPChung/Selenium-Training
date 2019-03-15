1ce1f717586d810085f7a419c6d4bb67d72b44e0
GeneCPChung <genecpc@gmail.com>
2019-03-14 20:58:07 -0400

    Installed Maven and began creating test classes

---
+++ MavenJava/src/test/java/geneC/app/RESTAPITest.java
@@ -0,0 +1,17 @@
+package geneC.app;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void posJira() {
+		System.out.println("posJira");
+	}
+
+	
+	@Test
+	public void DeleteTwitter() {
+		System.out.println("DeleteTwitter");
+	}
+}
