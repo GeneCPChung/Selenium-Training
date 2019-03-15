fef2e5548dd83f74ae4b57064662323bf8bad22d
GeneCPChung <genecpc@gmail.com>
2019-03-15 02:49:34 -0400

    Integrated TestNG with Maven

--- MavenJava/src/test/java/geneC/app/SeleniumTest.java
+++ MavenJava/src/test/java/geneC/app/SeleniumTest.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 
 public class SeleniumTest {
 	
+	//testng xml file from Maven
+	//how to execute all test cases from Test folder - mvn test
 	
 	@Test
 	public void BrowserAutomation() {
