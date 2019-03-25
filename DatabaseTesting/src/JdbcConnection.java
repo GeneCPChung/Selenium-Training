6af554639d3d75e66719307cfb4d9543d43f370d
GeneCPChung <genecpc@gmail.com>
2019-03-24 22:33:21 -0400

    created connnection and added information onto website with selenium

--- DatabaseTesting/src/JdbcConnection.java
+++ DatabaseTesting/src/JdbcConnection.java
@@ -1,14 +1,34 @@
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.sql.Connection;
 
 public class JdbcConnection {
 
 	public static void main(String[] args) throws SQLException {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\genec\\Desktop\\workspace\\geckodriver.exe");
 		// TODO Auto-generated method stub
 		String host = "localhost";
 		String port = "3306";
 		
-		DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/Qadbt", "root", "rootpassword");
+		Connection con = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/Qadbt", "root", "rootpassword");
+		
+		Statement s = con.createStatement();
+		
+		ResultSet rs = s.executeQuery("select * from Employeeinfo where name = 'nam';");
+		
+	while(	rs.next()) {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://login.salesforce.com/");
+		driver.findElement(By.xpath(".//input[@id='username']")).sendKeys(rs.getString("name"));
+		driver.findElement(By.xpath(".//input[@id='password']")).sendKeys(rs.getString("location"));
+	}
 	}
 
 }
